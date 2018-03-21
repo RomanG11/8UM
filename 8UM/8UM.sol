45f57b078f754d8e595caffdadda9d7c411437da
Roman <33402067+RomanG11@users.noreply.github.com>
2018-03-21 13:18:44 +0200

    Add files via upload

--- 8UM/8UM.sol
+++ 8UM/8UM.sol
@@ -36,8 +36,6 @@ contract Ownable {
 
   address public owner;
 
-  address public newOwner;
-
   modifier onlyOwner() {
     require(msg.sender == owner);
     _;
@@ -45,17 +43,6 @@ contract Ownable {
 
   function Ownable() public {
     owner = msg.sender;
-  }
-
-  function transferOwnership(address _newOwner) public onlyOwner {
-    require(_newOwner != address(0));
-    newOwner = _newOwner;
-  }
-
-  function acceptOwnership() public {
-    if (msg.sender == newOwner) {
-      owner = newOwner;
-    }
   }
 }
 
